c6f5a899354735f37be770e1dd5b2663f3920a0c
TT <test@Asperas-MacBook-Pro-2.local>
2020-07-15 16:41:56 -0700

    c

--- scripts/fileManager.py
+++ scripts/fileManager.py
@@ -6,15 +6,14 @@ import time
 import shutil
 tk = Tk()
 def setFilePath():
-    global filePath
     good = False
     while good == False:
+        global filePath
         filePath = simpledialog.askstring('set path',str('Set your file path'),parent=tk)
         if os.path.exists(filePath):
             good = True
         else:
             messagebox.showerror(title="Path Error", message="There is no such directory")
-
 def deleteFile():
     good = False
     while good == False:
@@ -26,7 +25,19 @@ def deleteFile():
             good = True
         else:
             messagebox.showerror(title="No such file", message="We could not find that file")
-
+def massFile():
+    good = False
+    while good == False:
+        folder = simpledialog.askstring('Folder',str('Choose a folder'),parent=tk)
+        total = filePath+str('/')+folder
+        if os.path.isdir(total):
+            if variable== OPTIONS[0]:
+                print('gi')
+                good = TRUE
+            else:
+                print('hi')
+        else:
+            messagebox.showerror(title="No such directory", message="We could not find that folder")
 def deleteFolder():
     good = False
     while good == False:
@@ -38,7 +49,6 @@ def deleteFolder():
             good = True
         else:
             messagebox.showerror(title="No such directory", message="We could not find that folder")
-
 def moveFile():
     good = FALSE
     while good == FALSE:
@@ -48,19 +58,32 @@ def moveFile():
         if os.path.exists(total):
             destination = simpledialog.askstring('destination',str('Enter a path to your destination'),parent=tk)
             if os.path.exists(destination):
-                newPath = shutil.move(total,destination)
-                messagebox.showinfo(title="Moved", message="File Moved")
-                good = TRUE
+                try:
+                    newPath = shutil.move(total,destination)
+                    messagebox.showinfo(title="Moved", message="File Moved")
+                    good = TRUE
+                except:
+                    messagebox.showerror(title="Error", message="File already exist in that directory")
             else:
                 messagebox.showerror(title="Invalid Destination", message="Could not find that destination")
         else:
             messagebox.showerror(title="No such file", message="There is no such file in that directory")
 
+
+
+OPTIONS = ['rename','delete','move']
+variable = StringVar(tk)
+variable.set(OPTIONS[0])
+tk.geometry("300x600")
 setDefaultPath = Button(tk,height = 10, width = 20, text='Set Default File Path',command = setFilePath)
-deleteFiles = Button(tk,height = 5, width = 10, text='Delete File',command = deleteFile)
-deleteFolders = Button(tk,height = 5, width = 10, text='Delete Folder',command = deleteFolder)
-moveFiles = Button(tk,height = 5, width = 10, text='Move File',command = moveFile)
+deleteFiles = Button(tk,height = 5, width = 20, text='Delete File',command = deleteFile)
+massFiles = Button(tk,height = 5, width = 20, text='Mass File Change',command = massFile)
+deleteFolders = Button(tk,height = 5, width = 20, text='Delete Folder',command = deleteFolder)
+moveFiles = Button(tk,height = 5, width = 20, text='Move File',command = moveFile)
+menuDrop = OptionMenu(tk,variable,*OPTIONS)
+menuDrop.pack()
 setDefaultPath.pack()
+massFiles.pack()
 deleteFiles.pack()
 deleteFolders.pack()
 moveFiles.pack()
